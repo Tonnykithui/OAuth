53b4d444f5afb69c0b8353f8bbe199db83e36215
iamtony <tonnykithui@gmail.com>
2022-05-03 11:41:07 +0300

    Added Services folder to hold Extension methods

--- OAuth/Startup.cs
+++ OAuth/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,6 +13,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using OAuth.DB;
 
 namespace OAuth
@@ -33,6 +36,26 @@ namespace OAuth
             {
                 options.UseSqlServer(Configuration.GetConnectionString("DefConnection"));
             });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = true,
+                    ValidAudience = "Users",
+                    ValidateIssuer = true,
+                    ValidIssuer = "https://localhost:44306",
+                    RequireExpirationTime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF32.GetBytes(Configuration["JwtConfig:security"]))
+                };
+            });
+            ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
